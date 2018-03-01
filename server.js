7346edfe589046241d1fb473c0b6479ceb02e6c8
Douglas Kim <dougkim@15solutions.com>
2018-03-01 12:46:10 -0800

    Implement GET, PUT and POST requests for manifests.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var uuid = require('uuid');
+var bodyParser = require('body-parser');
 
 // Create app
 var app = express();
@@ -9,67 +10,106 @@ var app = express();
 Route                         HTTP Verb Description
 -------------------------------------------------------------------------------------------
 /api/manifests                GET       Get all manifests
-/api/manifests                POST      Create a manifest - returns manifest id
-/api/manifests/:manifest_id   GET       Get manifest by id
-/api/manifests/:manifest_id   PUT       Update manifest with id
-/api/manifests/:manifest_id   DELETE    Delete manifest with id (currently not implemented)
+/api/manifests                POST      Create a manifest - returns manifest uri
+/api/manifests/:manifestId    GET       Get manifest by id
+/api/manifests/:manifestId    PUT       Update manifest with id
+/api/manifests/:manifestId    DELETE    Delete manifest with id (currently not implemented)
 -------------------------------------------------------------------------------------------
 */
 
+// ## CORS middleware
+// 
+// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+// app.use(express.methodOverride());
 
-// middleware
-app.use(function(req, res, next) {
-  // TODO: implement middleware
-  next();
-});
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 
-app.get('/', function (req, res) {
-  res.json({ data: { message: 'IIIF manifest store API' }}); 
-});
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.route('/api/manifests')
-
   // list all manifets
   .get(function(req, res) {
-    res.json({ data: { message: 'list all manifests' } });
+    // look up manifest list on the file system
+    var manifestFiles = fs.readdirSync('data/manifests');
+
+    var manifestUris = [];
+    manifestFiles.map((manifestFilename, index) => {
+      manifestUris.push({
+        uri: req.protocol + '://' + req.headers.host + '/manifests/' + manifestFilename
+      })
+    });
+
+    // generate JSON list with manifest uris
+    res.json({ manifests: manifestUris });
   }) 
 
   // create a manifest
   .post(function(req, res) {
-    
-    var manifest = '';
-    console.log(req);
+    // create a unique id for the manifest
+    var manifestId = uuid();
 
-    // TODO: assign uuid, store manifest, return created uuid
-    var manifest_id = uuid();
-    res.json({ data: { uri: manifest_id } });
-});
+    // store the manifest on the file system
+    fs.writeFileSync('data/manifests/' + manifestId, JSON.stringify(req.body));
 
-app.route('/api/manifests/:manifest_id')
+    // set the status code in the response
+    res.status(201);
 
-  // get manifest with id
-  .get(function(req, res) {
-    // TODO: return manifest with id
-    res.json({ data: { message: 'manifest JSON here' } });
-  })
-
-  // update manifst with id
-  .put(function(req, res) {
-    // TODO: update manifest with id
-    res.json({ data: { message: 'manifest updated' } });
-  })
-
-  // delete manifest with id
-  .delete(function(req, res) {
-    // Note: this is currently not implemented - we need authentication for this
-    res.json({ data: { message: 'deleting manifests is currently not supported' } });
+    // return the manifest uri
+    res.json({ uri: req.protocol + '://' + req.headers.host + '/api/manifests/' + manifestId });
   });
 
+app.route('/api/manifests/:manifestId')
+  // get manifest with id
+  .get(function(req, res) {
+    // get the manifest from the file system
+    var manifestData = fs.readFileSync('data/manifests/' + req.params.manifestId, 'utf8');
 
-// Set port
+    // return the manifest data
+    res.json(JSON.parse(manifestData));
+  })
+
+  // update an existing manifest with id
+  .put(function(req, res) {
+    var manifestPath = 'data/manifests/' + req.params.manifestId;
+    var statusCode = 200;
+
+    // check the file system to determine whether the resource exists
+    if(fs.existsSync(manifestPath)) {
+      // overwrite the manifest data on the file system
+      fs.writeFileSync(manifestPath, JSON.stringify(req.body));
+    } else {
+      statusCode = 404;
+    }
+
+    // set the status code in the response
+    res.status(statusCode);
+    res.json({ message: 'Manifest successfully updated' });
+  })
+
+  // delete an existing manifest with id
+  .delete(function(req, res) {
+    // Note: this is currently not implemented - we need authentication for this
+    res.json({ errorMessage: 'Deleting manifests is currently not supported' });
+  });
+
+// set port
 const PORT = process.env.PORT || 3001;
 
-// Listen on port
+// listen on port
 app.listen(PORT, function () {
   console.log('IIIF manifest store server is up on port ' + PORT);
 });
